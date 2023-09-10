5356c8ceefbaa83ed5cf3d1cac25c62c01772543
igortxra <igor.teixeira@cromai.com>
2023-09-10 12:51:28 -0300

    update install script

---
+++ .local/bin/menus/powermenu.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+source ~/.config/rofi/powermenu/type-1/powermenu.sh
---
+++ .local/bin/statusbar/battery-icon.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+capacity=$(cat /sys/class/power_supply/BAT1/capacity)
+quarter=$((capacity / 25))
+
+case $quarter in
+    [0-0.99])
+        icon=" "  # Ícone de bateria fraca
+        ;;
+    [1-1.99])
+        icon=" "  # Ícone de bateria média-baixa
+        ;;
+    [2-2.99])
+        icon=" "  # Ícone de bateria média-alta
+        ;;
+    [3-3.99])
+        icon=" "  # Ícone de bateria alta
+        ;;
+    4)
+        icon=" "  # Ícone de bateria completa
+        ;;
+    *)
+        icon=" "  # Ícone de bateria desconhecida
+        ;;
+esac
+
+echo "$icon" | tr -d "\n"

80949b50d624b8917d142c1cb9539f2cd656f83d
igortxra <igor.teixeira@cromai.com>
2024-01-02 01:24:10 -0300

    update install script

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -1,31 +1,43 @@
-echo "########## Remapping caps to super ##########"
-setxkbmap -option caps:super
-
+echo "#######################################"
 echo "########## Installing YAY... ##########"
+echo "#######################################"
+rm -rf yay
 git clone https://aur.archlinux.org/yay.git
 cd yay && makepkg -si && cd .. && rm -rf yay
 yay -Sy
 
+echo "#############################################"
 echo "########## Configuring Shell (zsh) ##########"
+echo "#############################################"
 yay -S zsh exa procs fzf  --noconfirm --quiet --needed
-# Zsh as default shell
-chsh -s /bin/zsh $(whoami)
+
 # Powerlevel10k
 yay -S --noconfirm --quiet --needed zsh-theme-powerlevel10k-git
+
 # Autosuggestions
+rm -rf ~/.zsh/zsh-autosuggestions 
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions 
 
+echo "######################################################"
 echo "########## Configuring Display Manager (Ly) ##########"
+echo "######################################################"
 yay -S Ly --noconfirm --quiet --needed 
 sudo systemctl enable ly.service
 
+echo "###########################################################"
 echo "########## Configuring Terminal Emulator (Kitty) ##########"
-yay -S kitty
+echo "###########################################################"
+yay -S kitty --noconfirm --quiet --needed
 
-echo "########## Configuring Window Manager (Qtile) ##########"
+
+echo "###########################################################"
+echo "########## Configuring Window Manager (Qtile) #############"
+echo "###########################################################"
+
 yay -S --noconfirm --quiet --needed python-pip 
 pip install xcffib==1.4 --break-system-packages
 pip install cairocffi==1.6 --break-system-packages
+yay -S --noconfirm --quiet --needed python-pywlroots
 yay -S --noconfirm --quiet --needed python-setuptools-git-versioning
 yay -S --noconfirm --quiet --needed qtile-git 
 yay -S --noconfirm --quiet --needed qtile-extras-git 
@@ -39,75 +51,114 @@ yay -S --noconfirm --quiet --needed python-psutil
 yay -S --noconfirm --quiet --needed python-dbus-next 
 yay -S --noconfirm --quiet --needed python-requests 
 
+
+echo "#####################################################"
 echo "########## Configuring App Launcher (Rofi) ##########"
+echo "#####################################################"
 yay -S --noconfirm --quiet --needed rofi
+rm rofi -rf
 git clone --depth=1 https://github.com/adi1090x/rofi.git
-cd rofi && chmod +x setup.sh && source ./setup.sh && cd ..
+cd rofi && chmod +x setup.sh && bash ./setup.sh && cd ..
 rm -rf ./rofi
 
-echo "########## Configuring Notification Daemon (Dunst) ##########"
+echo "#####################################################"
+echo "#### Configuring Notification Daemon (Dunst) ########"
+echo "#####################################################"
 yay -S --noconfirm --quiet --needed dunst
 
-echo "########## Configuring Browser (Qutebrowser) ##########"
+echo "#####################################################"
+echo "########## Configuring Browser (Qutebrowser) ########"
+echo "#####################################################"
 yay -S --quiet --needed --noconfirm qutebrowser 
+rm -rf ~/.config/qutebrowser/catppuccin
 git clone https://github.com/catppuccin/qutebrowser.git ~/.config/qutebrowser/catppuccin
 
+echo "############################################"
 echo "########## Install Picom Jonaburg ##########"
-yay -S --quiet --needed --noconfirm meson uthash python-setuptools
+echo "############################################"
+yay -S --quiet --needed --noconfirm meson uthash libev libconfig
+rm -rf picom
 git clone https://github.com/jonaburg/picom
 cd picom && meson --buildtype=release . build && ninja -C build && sudo ninja -C build install && cd ..
 rm -rf ./picom ./build
-
+ 
+echo "###################################################"
 echo "########## Install File Manager (Thunar) ##########"
+echo "###################################################"
 yay -S --quiet --needed --noconfirm thunar
 
+echo "#################################################################"
 echo "########## Install Screen Utils (arandr and autorandr) ##########"
+echo "#################################################################"
 yay -S --quiet --needed --noconfirm arandr autorandr
 
+echo "###############################################################"
 echo "########## Install Clipboard Utils (copyq and xclip) ##########"
+echo "###############################################################"
 yay -S --quiet --needed --noconfirm copyq
 yay -S --quiet --needed --noconfirm xclip
 
-echo "########## Install Nerd Fonts (Iosevka Nerd) ##########"
+echo "###################################################"
+echo "########## Install Nerd Fonts (Iosevka Nerd) ######"
+echo "###################################################"
 yay -S --quiet --needed --noconfirm ttf-iosevka-nerd
 
+echo "###############################"
 echo "########## Inetutils ##########"
+echo "###############################"
 yay -S --noconfirm --quiet --needed inetutils-git
 
-echo "########## Make use of dotfiles ##########"
-echo ".dotfiles" >> .gitignore
-git clone --bare https://github.com/$USER/dotfiles.git $HOME/.dotfiles
-alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
-dotfiles config --local status.showUntrackedFiles no
-dotfiles checkout
-
-echo "########## Configuring Wallpaper (Nitrogen) ##########"
+echo "###################################################"
+echo "########## Configuring Wallpaper (Nitrogen) #######"
+echo "###################################################"
 yay -S --noconfirm --quiet --needed nitrogen
-nitrogen --set-auto ~/Wallpapers/default.png
 
+echo "##########################################"
 echo "########## Creating user dirs ############"
+echo "##########################################"
 yay -S --quiet --needed --noconfirm xdg-user-dirs
 xdg-user-dirs-update --force
 
+echo "################################################################"
 echo "########## Installing themes (Dracula and Catppuccin) ##########"
+echo "#####################################################@##########"
 yay -S --quiet --needed --noconfirm lxappearance
 yay -S --quiet --needed --noconfirm dracula-cursos-git
 yay -S --quiet --needed --noconfirm dracula-icons-git
 yay -S --quiet --needed --noconfirm catppuccin-gtk-theme-mocha
 
 # Opcionais
-
-echo "########## Install Developer Tools ##########"
+echo "#####################################"
+echo "########## Install ASDF-VM ##########"
+echo "#####################################"
 yay -S --quiet --needed --noconfirm asdf-vm
-yay -S --quiet --needed --noconfirm postman-bin
 
+echo "##########################################"
 echo "########## Installing AstroNvim ##########"
+echo "##########################################"
 yay -S --quiet --needed --noconfirm neovim npm fzf
+rm -rf ~/.config/nvim
 git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/nvim
 
+echo "########################################"
 echo "########## Installing Spotify ##########"
+echo "########################################"
 yay -S --quiet --needed --noconfirm spotify
 
+echo "#################################################"
 echo "########## Installing Notes (Obsidian) ##########"
+echo "#################################################"
 yay -S --quiet --needed --noconfirm obsidian
 
+echo "###################################################"
+echo "########## Make use of dotfiles ###################"
+echo "###################################################"
+echo ".dotfiles" >> .gitignore
+rm -rf $HOME/.dotfiles
+git clone --bare https://github.com/$USER/dotfiles.git $HOME/.dotfiles
+alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+dotfiles config --local status.showUntrackedFiles no
+dotfiles checkout -f
+
+# Zsh as default shell
+chsh -s /bin/zsh $(whoami)

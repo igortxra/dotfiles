34c43f41d0041cfc3b04f1a565df65a58ab8dc03
igortxra <igor.teixeira@cromai.com>
2024-01-03 14:07:38 -0300

    fix: battery icon script

--- .local/bin/statusbar/battery-icon.sh
+++ .local/bin/statusbar/battery-icon.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 
-capacity=$(cat /sys/class/power_supply/BAT1/capacity)
-
-if [ -z "$capacity" ]
-then
-    capacity=10000
+# Verifica se o arquivo de capacidade da bateria existe
+if [ -e "/sys/class/power_supply/BAT1/capacity" ]; then
+    # Lê a capacidade da bateria do arquivo
+    capacity=$(cat /sys/class/power_supply/BAT1/capacity)
+else
+    capacity=10000  # Define um valor padrão caso não seja possível ler a capacidade
 fi
 
-quarter=$((capacity / 25))
+# Calcula o quarto da capacidade da bateria
+quarter=$(echo "scale=0; $capacity / 25" | bc)
 
+# Exibe o ícone com base no quarto da capacidade
 case $quarter in
-    [0-0.99])
+    0)
         icon=" "  # Ícone de bateria fraca
         ;;
-    [1-1.99])
+    1)
         icon=" "  # Ícone de bateria média-baixa
         ;;
-    [2-2.99])
+    2)
         icon=" "  # Ícone de bateria média-alta
         ;;
-    [3-3.99])
+    3)
         icon=" "  # Ícone de bateria alta
         ;;
-    4)
+    4 | 5 | 6 | 7 | 8)
         icon=" "  # Ícone de bateria completa
         ;;
     *)
-        icon=""  # Ícone de bateria desconhecida
+        icon="A"  # Ícone de bateria desconhecida
         ;;
 esac
 

d741d4c2f9313b5c559a32cf0e28d971c6d8b10e
igortxra <igorxteixeira@gmail.com>
2024-01-29 12:57:31 -0300

    unify pkg-list and foreign-pkg-list

--- Scripts/utils/save-explicit-installed-packages.sh
+++ Scripts/utils/save-explicit-installed-packages.sh
@@ -2,4 +2,4 @@
 
 dunstify -a System -t 5000 "Saving explicit installed packages" "You can see the result in $HOME/Setup/"
 pacman -Qqen > ~/Setup/pkg-list.txt
-pacman -Qqem > ~/Setup/foreign-pkg-list.txt
+pacman -Qqem | sed 's/$/ # AUR/' >> ~/Setup/pkg-list.txt
